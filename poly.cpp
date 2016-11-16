a7eb0d106aebc9b6f6d8f8dc578effbb6e103266
Ying Gao <gaoying@umich.edu>
2016-11-15 21:31:18 -0500

    first commit --class

---
+++ poly.cpp
@@ -0,0 +1,47 @@
+// here we implement the class of circle and triangle from shapes
+#include<iostream>
+using namespace std;
+class Shape{
+protected: 
+	int width;
+	int height;
+	int color;
+public:
+	Shape(int a=0, int b=0, int c=0){
+		width=a;
+		height=b;
+		color=c;
+	}
+	int getArea(){
+		return width*height;
+	}
+	void printArea(){
+		cout<<"the area is "<<getArea()<<endl;
+	}
+	void printColor(){
+		cout<<"the color is "<<color<<endl;
+	}
+};
+
+class Triangle: public Shape
+{
+public:
+	Triangle(int a=0, int b=0, int c=0):Shape(a,b,c){}
+	int getArea(){
+		return width*height/2;
+	}
+	void printArea(){
+		cout<<"the area is "<<getArea()<<endl;
+	}
+	/* data */
+};
+
+int main(){
+	Triangle t(1,8,2);
+	Shape s(1,8,2);
+	s.printArea();
+	t.printArea();
+	cout<<"triangle area is "<<t.getArea()<<endl;
+	cout<<"default area is "<<s.getArea()<<endl;
+	return 0;
+}
